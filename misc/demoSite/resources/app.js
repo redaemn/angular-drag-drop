ac635ebff008329d4ad1fbb9eb95c4a030430cdd
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2013-12-10 10:48:49 -0500

    feat(angular-drag-drop): fix demo code indentation

--- misc/demoSite/resources/app.js
+++ misc/demoSite/resources/app.js
@@ -1,72 +1,76 @@
 angular.module('app', ['angular-drag-drop']);
 
 function pageController($scope, dragdropService) {
-    
-    var previousDay;
-    
-    $scope.appointment1 = {id: 1};
-    $scope.appointment2 = {id: 2};
-    
-    $scope.days = [
-        [$scope.appointment1, $scope.appointment2],
-        [],
-        []
-    ];
-    
-    $scope.click = function() {
-        $scope.appointment1.id += 1;
-    };
-    
-    $scope.draggableEnter = function (draggable, droppable) {
-        //debugger;
-        add(draggable, droppable);
-    };
-    
-    $scope.draggableLeave = function (draggable, droppable) {
-        remove(draggable, droppable);
-    };
-    
-    $scope.dragStart = function (draggable, droppable) {
-        var idx;
-        
-        idx = droppable.indexOf(draggable);
-        
-        if (idx >= 0) {
-            previousDay = droppable;
-        }
-    };
-    
-    $scope.draggableDrop = function (draggable, droppable) {
-        add(draggable, droppable);
-    };
-    
-    $scope.revert = function() {
-        //debugger;
-        var droppableHover = dragdropService.droppableHover();
-        var dragging = dragdropService.dragging();
-        
-        return !droppableHover || droppableHover.indexOf(dragging) >= 0;
-    };
-    
-    function add(draggable, droppable) {
-        //debugger;
-        if (droppable.indexOf(draggable) < 0) {
-            remove(draggable, previousDay);
-            droppable.push(draggable);
-        }
-        
-        $scope.$apply();
+
+  var previousDay;
+
+  $scope.appointment1 = {
+    id: 1
+  };
+  $scope.appointment2 = {
+    id: 2
+  };
+
+  $scope.days = [
+    [$scope.appointment1, $scope.appointment2],
+    [],
+    []
+  ];
+
+  $scope.click = function() {
+    $scope.appointment1.id += 1;
+  };
+
+  $scope.draggableEnter = function(draggable, droppable) {
+    //debugger;
+    add(draggable, droppable);
+  };
+
+  $scope.draggableLeave = function(draggable, droppable) {
+    remove(draggable, droppable);
+  };
+
+  $scope.dragStart = function(draggable, droppable) {
+    var idx;
+
+    idx = droppable.indexOf(draggable);
+
+    if (idx >= 0) {
+      previousDay = droppable;
     }
-    
-    function remove(draggable, droppable) {
-        var idx;
-        
-        idx = droppable.indexOf(draggable);
-        
-        if (idx >= 0) {
-            droppable.splice(idx, 1);
-        }
-        
-        $scope.$apply();
+  };
+
+  $scope.draggableDrop = function(draggable, droppable) {
+    add(draggable, droppable);
+  };
+
+  $scope.revert = function() {
+    //debugger;
+    var droppableHover = dragdropService.droppableHover();
+    var dragging = dragdropService.dragging();
+
+    return !droppableHover || droppableHover.indexOf(dragging) >= 0;
+  };
+
+  function add(draggable, droppable) {
+    //debugger;
+    if (droppable.indexOf(draggable) < 0) {
+      remove(draggable, previousDay);
+      droppable.push(draggable);
     }
+
+    $scope.$apply();
+  }
+
+  function remove(draggable, droppable) {
+    var idx;
+
+    idx = droppable.indexOf(draggable);
+
+    if (idx >= 0) {
+      droppable.splice(idx, 1);
+    }
+
+    $scope.$apply();
+  }
 }
